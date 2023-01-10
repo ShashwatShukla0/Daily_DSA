a539abb8ed56b87898d32bb6962ffe83ae0ac02d
ShashwatShukla0 <sskrish786@gmail.com>
2023-01-10 23:47:27 +0530

    Commit

---
+++ Array/SubarraySum.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n,sum;
+    cin>>n;
+    cin>>sum;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    for(int i=0;i<n;i++){
+        int currSum= arr[i];
+
+        if(currSum==sum) {
+            cout<<"Sum found at index: "<<i<<endl;
+            return 1;
+        } else {
+            for(int j=i+1;j<n;j++){
+                currSum+= arr[j];
+
+                if(currSum==sum){
+                    cout<<"Sum found at index: "<<i<<" & "<<j;
+                }
+            }
+        }
+    }
+}
