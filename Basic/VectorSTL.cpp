45c3a1bcfbebd9fa53c2e810b24af66c4247343c
ShashwatShukla0 <sskrish786@gmail.com>
2023-01-25 19:18:55 +0530

    Commit

---
+++ Basic/VectorSTL.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> arr;
+    arr.push_back(3);
+    arr.push_back(4);
+    arr.push_back(5);
+    arr.push_back(6);
+    arr.push_back(7);
+
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
+}
